55941e42799c7d9a9e8f78c7177b3728af221a27
Anders Stromberg <coltlaboy@yahoo.se>
2019-09-24 13:13:04 +0200

    Minor update to js and html files

--- ajaxCall.js
+++ ajaxCall.js
@@ -1,81 +1,75 @@
-
 //POST REQUEST
+$(document).ready(function() {
+  $('#postMessage').click(e => {
+    e.preventDefault();
 
-$(document).ready(function(){
-    $('#postMessage').click(function(e){
-        e.preventDefault();
+    //serialize form data
+    var url = $('form').serialize();
 
-        //serialize form data
-        var url = $('form').serialize();
+    //function to turn url to an object
+    function getUrlVars(url) {
+      var hash;
+      var myJson = {};
+      var hashes = url.slice(url.indexOf('?') + 1).split('&');
+      for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        myJson[hash[0]] = hash[1];
+      }
+      return JSON.stringify(myJson);
+    }
 
-        //function to turn url to an object
-        function getUrlVars(url) {
-            var hash;
-            var myJson = {};
-            var hashes = url.slice(url.indexOf('?') + 1).split('&');
-            for (var i = 0; i < hashes.length; i++) {
-                hash = hashes[i].split('=');
-                myJson[hash[0]] = hash[1];
-            }
-            return JSON.stringify(myJson);
-        }
+    //pass serialized data to function
+    var test = getUrlVars(url);
 
-        //pass serialized data to function
-        var test = getUrlVars(url);
+    //post with ajax
+    $.ajax({
+      type: 'POST',
+      // url: "/Work folders/OOP php/RESTFUL traversy/php_rest_myblog/api/post/create.php",
+      url: 'api/post/create.php',
+      data: test,
+      ContentType: 'application/json',
 
-        //post with ajax
-        $.ajax({
-            type:"POST",
-            url: "/Work folders/OOP php/RESTFUL traversy/php_rest_myblog/api/post/create.php",
-            data: test,
-            ContentType:"application/json",
-
-            success:function(){
-                alert('successfully posted');
-            },
-            error:function(){
-                alert('Could not be posted');
-            }
-
-        });
+      success: () => {
+        alert('successfully posted');
+      },
+      error: () => {
+        alert('Could not be posted');
+      }
     });
+  });
 });
-    
 
 //GET REQUEST
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('getMessage').onclick = () => {
+    var req;
+    req = new XMLHttpRequest();
+    req.open('GET', 'api/post/read.php', true);
+    req.send();
 
-  document.addEventListener('DOMContentLoaded',function(){
-  document.getElementById('getMessage').onclick=function(){
-       
-       var req;
-       req=new XMLHttpRequest();
-       req.open("GET", '/Work folders/OOP php/RESTFUL traversy/php_rest_myblog/api/post/read.php',true);
-       req.send();
-      
-       req.onload=function(){
-       var json=JSON.parse(req.responseText);
+    req.onload = () => {
+      var json = JSON.parse(req.responseText);
 
-       //limit data called
-       var son = json.filter(function(val) {
-              return (val.id >= 4);  
-          });
+      //limit data called
+      var son = json.filter(val => {
+        return val.id >= 4;
+      });
 
-      var html = "";
+      var html = '';
 
       //loop and display data
-      son.forEach(function(val) {
-          var keys = Object.keys(val);
+      son.forEach(val => {
+        var keys = Object.keys(val);
 
-          html += "<div class = 'cat'>";
-              keys.forEach(function(key) {
-              html += "<strong>" + key + "</strong>: " + val[key] + "<br>";
-              });
-          html += "</div><br>";
+        html += "<div class = 'cat'>";
+        keys.forEach(key => {
+          html += '<strong>' + key + '</strong>: ' + val[key] + '<br>';
+        });
+        html += '</div><br>';
       });
 
       //append in message class
-      document.getElementsByClassName('message')[0].innerHTML=html;         
-      };
+      document.getElementsByClassName('message')[0].innerHTML = html;
     };
-  });
-  
+  };
+});
